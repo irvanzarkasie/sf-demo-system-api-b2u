c1da9121cb09452857e90630d5d2166f1b6aa91f
irvan <irvan.ramadhan@expecc.com>
2024-05-19 19:11:49 +0700

    Initial commit

---
+++ src/b2u_system_api.py
@@ -0,0 +1,54 @@
+import sys
+import json
+from flask import Flask, request, jsonify, make_response, Response
+import pprint
+import logging
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
+import uuid
+import socket
+from datetime import datetime, timedelta
+import os
+import urllib3
+
+app = Flask(__name__)
+
+# CONSTANTS
+api_host = socket.gethostname()
+api_port = 35020
+api_id = "b2u_system_api"
+
+# Work directory setup
+script_dir = os.path.dirname(os.path.realpath(__file__))
+home_dir = "/".join(script_dir.split("/")[:-1])
+log_dir = "{home_dir}/logs".format(home_dir=home_dir)
+
+# HTTP connection pool
+http = urllib3.PoolManager()
+
+@app.route('/sys/b2u/booking/routes', methods=['GET'])
+def getRoutes():
+  # Parse arguments
+  args = request.args
+  departure_code = args.get("departure_code", "")
+  destination_code = args.get("destination_code", "")
+
+  data = """
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://www.example.org/Bookings/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <tns:getRoutes>
+         <tns:departureCode></tns:departureCode>
+         <tns:destinationCode></tns:destinationCode>
+      </tns:getRoutes>
+   </soapenv:Body>
+</soapenv:Envelope>
+  """
+  resp = http.request("POST", "http://168.119.225.15:38000/getRoutes", body=data, headers={'Content-Type': 'application/xml'})
+  print(resp.data)
+
+  return jsonify({})
+# end def
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=api_port)
+# end if
